9516a90537a94d5a6a772e0cf8c8da4bd2a5cd36
JRScelza <jerome.scelza@mssm.edu>
2018-04-03 14:05:56 -0400

    Updated Table

--- table_to_mark.py
+++ table_to_mark.py
@@ -1,53 +1,53 @@
 from tabulate import tabulate
 import pandas as pd
 
-file_name = './clean_table_final.csv'
+file_name = './final_table.csv'
 
 df_unformatted_load = pd.read_csv(file_name, index_col=0)
 
-start_keys = ['Twitter' , 'Relevant to DataCoin']
-df = df_unformatted_load[start_keys]
-df = df.fillna(value = '-')
+main_table_keys = ['Site' , 'Name' , 'Category' , 'Location', 'Code' ,'Demo','TS']
+df_main = df_unformatted_load[main_table_keys]
+df_main = df_main.fillna(value = '-')
 
 
-# df = df_unformatted_load[df_unformatted_load.columns[0:11]]
+# # df = df_unformatted_load[df_unformatted_load.columns[0:11]]
 
-blockchain_dict = df.to_dict('index')
+# blockchain_dict = df.to_dict('index')
 
-blockchain_companies = list(df.index)
-blockchain_keys = list(df.columns)
+# blockchain_companies = list(df.index)
+# blockchain_keys = list(df.columns)
 
 
 
-#Markdown Params.
-git_logo_source = "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"
+# #Markdown Params.
+# git_logo_source = "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"
 
-def section_name(name):
-    s = name.lower()
-    section_header = '(#' + s + ')'
-    return(section_header)
+# def section_name(name):
+#     s = name.lower()
+#     section_header = '(#' + s + ')'
+#     return(section_header)
 
-# white_paper_emoji = 
-# computer_emoji =
-# smartphone_emoji = 
+# # white_paper_emoji = 
+# # computer_emoji =
+# # smartphone_emoji = 
 
-# logo_hyperlink = [<img src="http://www.google.com.au/images/nav_logo7.png">](http://google.com.au/)
+# # logo_hyperlink = [<img src="http://www.google.com.au/images/nav_logo7.png">](http://google.com.au/)
 
-#There are github markdown emoji's take note https://gist.github.com/rxaviers/7360908
+# #There are github markdown emoji's take note https://gist.github.com/rxaviers/7360908
 
-#Making Markdown
+# #Making Markdown
 
-column_count = len(df.columns)
-bar_count = column_count + 1
+# column_count = len(df.columns)
+# bar_count = column_count + 1
 
-headerSeparator = '|:--' * column_count + '|'
+# headerSeparator = '|:--' * column_count + '|'
 
-table = []
-for i in range(len(df)):
-    table.append(list(df.iloc[i]))
+# table = []
+# for i in range(len(df)):
+#     table.append(list(df.iloc[i]))
 
-headers = list(df.columns)
+# headers = list(df.columns)
 
-# print(tabulate(table, headers, tablefmt="pipe"))
+# # print(tabulate(table, headers, tablefmt="pipe"))
 
 
